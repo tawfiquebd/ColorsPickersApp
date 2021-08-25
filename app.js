18a7045200ac3467b8a699d6a524b436818905d2
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-25 23:36:22 +0600

    Checking contrast

--- app.js
+++ app.js
@@ -31,6 +31,19 @@ function randomColors() {
     // Add the color to the bg
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+
+    // Check for contrast
+    checkTextContrast(randomColor, hexText);
   });
 }
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
 randomColors();
