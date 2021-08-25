622f36783a2f07acffe38a90771d954b49e16d8b
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-25 23:21:10 +0600

    Generate first color

---
+++ app.js
@@ -0,0 +1,36 @@
+// Global selections and variables
+
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll("input[type='range']");
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+// Functions
+
+// Color generator our function
+// function generateHex() {
+//   const letters = "0123456789ABCDEF";
+//   let hash = "#";
+//   for (let i = 0; i < 6; i++) {
+//     hash += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return hash;
+// }
+
+// Color generator using chroma js library
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+    // Add the color to the bg
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+randomColors();
