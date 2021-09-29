406f16193e846769288829f91eeaff5bfce3720b
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-09-29 20:10:07 +0600

    Coloring our inputs

--- app.js
+++ app.js
@@ -34,6 +34,15 @@ function randomColors() {
 
     // Check for contrast
     checkTextContrast(randomColor, hexText);
+
+    // Initial colorize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -44,6 +53,27 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  // scale staturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  // Scale Brightness
+  const midBright = color.set(`hsl.l`, 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  // Update input colors
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+    0
+  )}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`;
 }
 
 randomColors();
