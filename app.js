78dcb60810f4bf0833075970d67707ee127fa5d1
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-10-03 12:52:33 +0600

    Generate button

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const sliders = document.querySelectorAll("input[type='range']");
 const currentHexes = document.querySelectorAll(".color h2");
 const popup = document.querySelector(".copy-container");
 const adjustButton = document.querySelectorAll(".adjust");
+const lockButton = document.querySelectorAll(".lock");
 const closeAdjustments = document.querySelectorAll(".close-adjustment");
 const sliderContainers = document.querySelectorAll(".sliders");
 let initialColors;
 
 // Add our event Listeners
+generateBtn.addEventListener("click", randomColors);
 sliders.forEach((slider) => {
   slider.addEventListener("input", hslControls);
 });
@@ -89,6 +91,11 @@ function randomColors() {
   });
   // Reset Input
   resetInput();
+  // Check for button contrast
+  adjustButton.forEach((button, index) => {
+    checkTextContrast(initialColors[index], button);
+    checkTextContrast(initialColors[index], lockButton[index]);
+  });
 }
 
 function checkTextContrast(color, text) {
