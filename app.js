0e6a64bed74594af7e7b157d245fe631faabc0aa
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-10-04 21:55:47 +0600

    Picking from Library

--- app.js
+++ app.js
@@ -295,6 +295,21 @@ function savePalette(e) {
   paletteBtn.classList.add(paletteObj.nr);
   paletteBtn.innerText = "Select";
 
+  // Attach event to the btn
+  paletteBtn.addEventListener("click", (e) => {
+    closeLibrary();
+    const paletteIndex = e.target.classList[1];
+    initialColors = [];
+    savedPalettes[paletteIndex].colors.forEach((color, index) => {
+      initialColors.push(color);
+      colorDivs[index].style.backgroundColor = color;
+      const text = colorDivs[index].children[0];
+      checkTextContrast(color, text);
+      updateTextUI(index);
+    });
+    libraryInputUpdate();
+  });
+
   // Append to Library
   palette.appendChild(title);
   palette.appendChild(preview);
